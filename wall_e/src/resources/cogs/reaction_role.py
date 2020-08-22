c2d8269b16a01046d11807bd8fc0d6eba0934428
VJ <vijenderbakhshi@gmail.com>
2020-08-21 22:57:25 -0700

    minor update between computers

--- wall_e/src/resources/cogs/reaction_role.py
+++ wall_e/src/resources/cogs/reaction_role.py
@@ -3,7 +3,9 @@ from discord.ext import commands
 import logging
 logger = logging.getLogger('wall_e')
 
+
 class ReactionRole(commands.Cog):
+
     def __init__(self, bot, config):
         self.bot = bot
         self.config = config
@@ -16,41 +18,25 @@ class ReactionRole(commands.Cog):
 
         return check
 
-    # async def getInput(self, ctx, prompt):
-
-
     @commands.command()
     async def reactrole(self, ctx):
         logger.info("[ReactionRole reactRole()] starting interactive process to create react role embed")
-        # await ctx.send("Enter your prompt for reaction role")
         ch = self.check(ctx.author, ctx.channel)
 
         '''TODO:
             - array of all prompts for inputs, then foreach them
             - need some way to identify optional prompts
             - special case for content fields
+        need:
+            - channel to have react embed
+            - title w/ call to action prompt            
+            - emoji, role, and what to list as in embed
+            - optional colour 
+            - role and channel can be both the mention or just name
         '''
 
-        em = {
-            'title':'',
-            'description':'',
-            'colour':'',
-            'thumbnail':'',
-            'content':'',
-            'footer':''
-        }
-
-        for key in em:
-            await ctx.send('Give input for {}'.format(key))
-            em[key] = await self.bot.wait_for('message', check=ch)
-            em[key] = em[key].content
-            if em[key] == 'none':
-                await ctx.send('react embed creation aborted')
-                return
-
         # abort at anytime with keyword 'exit' alone
-        author = ctx.author
-        icon = ctx.author.avatar_url
+ 
         # title = await self.bot.wait_for('message', check=ch)
         # if(title == 'exit'):
         #     await ctx.send('react embed creation aborted')
