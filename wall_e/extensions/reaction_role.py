67e1ce8da491271850fcd9aa32372f87254321c9
VJ <vijenderbakhshi@gmail.com>
2025-06-28 20:01:40 -0700

    addned new extension, ReactionRole, with some boilerplate code

---
+++ wall_e/extensions/reaction_role.py
@@ -0,0 +1,51 @@
+import discord
+from discord.ext import commands
+
+from utilities.global_vars import bot, wall_e_config
+
+from utilities.file_uploading import start_file_uploading
+from utilities.setup_logger import Loggers
+
+
+class ReactionRole(commands.Cog):
+
+    def __init__(self):
+        log_info = Loggers.get_logger(logger_name='ReactionRole')
+        self.logger = log_info[0]
+        self.debug_log_file_absolute_path = log_info[1]
+        self.warn_log_file_absolute_path = log_info[2]
+        self.error_log_file_absolute_path = log_info[3]
+        self.logger.info("[ReactionRole __init__()] initializing ReactionRole")
+        self.guild: discord.Guild | None = None
+
+    @commands.Cog.listener(name="on_ready")
+    async def get_guild(self):
+        self.guild = bot.guilds[0]
+
+    @commands.Cog.listener(name="on_ready")
+    async def upload_debug_logs(self):
+        while self.guild is None:
+            await asyncio.sleep(2)
+        await start_file_uploading(
+            self.logger, self.guild, bot, wall_e_config, self.debug_log_file_absolute_path, "reaction_role_debug"
+        )
+
+    @commands.Cog.listener(name="on_ready")
+    async def upload_warn_logs(self):
+        while self.guild is None:
+            await asyncio.sleep(2)
+        await start_file_uploading(
+            self.logger, self.guild, bot, wall_e_config, self.warn_log_file_absolute_path, "reaction_role_warn"
+        )
+
+    @commands.Cog.listener(name="on_ready")
+    async def upload_error_logs(self):
+        while self.guild is None:
+            await asyncio.sleep(2)
+        await start_file_uploading(
+            self.logger, self.guild, bot, wall_e_config, self.error_log_file_absolute_path, "reaction_role_error"
+        )
+
+
+async def setup(bot):
+    await bot.add_cog(ReactionRole())
